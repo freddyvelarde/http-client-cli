a7875f093385e173228eccc6d75728d7e5b76b2d
Freddy Velarde <freddyvelarde59@gmail.com>
2023-02-17 21:10:09 -0400

    :recycle: REFACTOR: improving printing json format code.

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,3 +1,4 @@
+use colored_json::ToColoredJson;
 use serde_json::to_string_pretty;
 use serde_json::{Result, Value};
 
@@ -10,11 +11,26 @@ pub fn normalize_header(headers: &Vec<String>) -> Vec<Vec<String>> {
     return header_splited;
 }
 
-pub fn print_in_json_format(data: String) -> Result<()> {
+pub fn print_pretty_response(data: String, status: String) -> Result<()> {
+    // const COLOR_RED: &str = "\x1b[31m";
+    const COLOR_GREEN: &str = "\x1b[32m";
+    const COLOR_RESET: &str = "\x1b[0m";
+    const COLOR_YELLOW: &str = "\x1b[33m";
+
     let v: Value = serde_json::from_str(&format!(r#"{}"#, data.to_string()))?;
 
     let pretty_json = to_string_pretty(&v)?;
-    println!("{}", pretty_json);
+
+    println!(
+        "{}{}{}",
+        COLOR_GREEN, "---------------- STATUS ----------------", COLOR_RESET
+    );
+    println!("{}STATUS: {}{}", COLOR_YELLOW, status, COLOR_RESET);
+    println!(
+        "{}{}{}",
+        COLOR_GREEN, "--------------- RESPONSE ---------------", COLOR_RESET
+    );
+    println!("{}", pretty_json.to_colored_json_auto()?);
 
     Ok(())
 }
